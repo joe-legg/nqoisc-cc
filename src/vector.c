7033d3fd5bc4870a22a98d7219b596a97cf6dd01
Joe Legg <joelegg3@gmail.com>
2020-01-20 15:19:34 +0000

    remove malloc_or_die dependency in vector.c

--- src/vector.c
+++ src/vector.c
@@ -1,13 +1,25 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "vector.h"
-#include "malloc_or_die.h"
 
 Vector *new_vector()
 {
-    Vector *vector = malloc_or_die(sizeof(Vector));
+    Vector *vector = malloc(sizeof(Vector));
+
+    if (vector == NULL) {
+        printf("vector: error allocating memory.");
+        abort();
+    }
+
     // Initial capacity
     vector->capacity = 4;
-    vector->items = malloc_or_die(sizeof(void *) * vector->capacity);
+    vector->items = malloc(sizeof(void *) * vector->capacity);
+
+    if (vector->items == NULL) {
+        printf("vector: error allocating memory.");
+        abort();
+    }
+
     vector->length = 0;
     return vector;
 }
@@ -20,6 +32,11 @@ void vector_append(Vector *vect, void *item)
     if (vect->length == vect->capacity) {
         vect->capacity *= 2;
         vect->items = realloc(vect->items, vect->capacity * sizeof(void *));
+
+        if (vect->items == NULL) {
+            printf("vector: error reallocating memory.");
+            abort();
+        }
     }
 
     vect->items[vect->length++] = item;
