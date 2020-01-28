4cb7a1d4d62121bfb3b87c8137454a2fb763fbc1
Joe Legg <joelegg3@gmail.com>
2020-01-28 18:04:06 +0000

    Delete nqoisc_toolchain as it is now contained in it's own repository.

--- nqoisc_toolchain/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc -Wall -g -o bin/nqoisc-sim simulator.c
-gcc -Wall -g -o bin/nqoisc-cat cat.c
-gcc -Wall -g -o bin/nqoisc-asm asm.c ../src/vector.c
--- nqoisc_toolchain/install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mv bin/* /usr/local/bin
